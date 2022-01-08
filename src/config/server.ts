41d3469a8acce99bbcbea12a9c7a4d676b79eb41
Davor Pernjek <pernjek.davor12@gmail.com>
2022-01-08 11:12:29 +0100

    add test endpoint

--- src/config/server.ts
+++ src/config/server.ts
@@ -2,10 +2,13 @@ import express, { Request, Response } from "express";
 const app = express();
 const PORT: string | number = process.env.PORT || 5000;
 
-app.use("*", (req, res) => {
-  res.send("<h1>Welcome to your simple server! Awesome right</h1>");
-});
 app.get("/", async (req: Request, res: Response): Promise<Response> => {
+  return res.status(200).send({
+    message: "Hello World!",
+  });
+});
+
+app.get("/test", async (req: Request, res: Response): Promise<Response> => {
   return res.status(200).send({
     message: "Hello World!",
   });
